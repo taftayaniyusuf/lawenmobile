2b35ba2e4c4d1d044a46c117df9bd746e9fe9bd3
rasyidmm <rasyidmaulidmajid@gmail.com>
2018-07-23 02:43:51 +0700

    memusahkah controller

---
+++ MobileProject/src/main/java/com/example/demo/controller/AkunBankController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.AkunBank;
+import com.example.demo.service.AkunBankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AkunBankController {
+    @Autowired
+    AkunBankService akunBankService;
+    @RequestMapping(value = "/insertakunbank",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean insertAkunBank(@RequestParam("id")long id, @RequestParam("nama_bank")String nama_bank){
+        AkunBank akunBank =  new AkunBank();
+        akunBank.setId(id);
+        akunBank.setNama_bank(nama_bank);
+        akunBankService.SaveOrUpdate(akunBank);
+        return true;
+    }
+    @RequestMapping(value = "/updateakunbank",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean updateAkunBank(@RequestParam("id")long id,@RequestParam("nama_bank")String nama_bank){
+        AkunBank akunBank =akunBankService.getById(id);
+        akunBank.setId(id);
+        akunBank.setNama_bank(nama_bank);
+        akunBankService.SaveOrUpdate(akunBank);
+        return true;
+    }
+    @RequestMapping(value = "/deleteakunbank",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean deleteAkunBank(@RequestParam("id")long id){
+        AkunBank akunBank =akunBankService.getById(id);
+        akunBankService.deleteAkunBank(akunBank);
+        return true;
+    }
+}
---
+++ MobileProject/src/main/java/com/example/demo/controller/TeamController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Team;
+import com.example.demo.service.TeamServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+    @Autowired
+    TeamServices teamServices;
+    @RequestMapping(value = "/insertteam",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean insertteam(@RequestParam("id")long id, @RequestParam("nama_team") String nama_team){
+        Team team = new Team();
+        team.setId(id);
+        team.setNama_team(nama_team);
+        teamServices.SaveOrUpdate(team);
+        return true;
+    }
+    @RequestMapping(value = "/updateteam",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean updateteam(@RequestParam("id")long id,@RequestParam("nama_team") String nama_team){
+        Team team = teamServices.getById(id);
+        team.setId(id);
+        team.setNama_team(nama_team);
+        teamServices.SaveOrUpdate(team);
+        return true;
+    }
+    @RequestMapping(value = "/deleteteam",method = RequestMethod.POST,headers = "Accept=application/json")
+    public boolean deleteteam(@RequestParam("id")long id){
+        Team team = teamServices.getById(id);
+        teamServices.deleteTeam(team);
+        return true;
+    }
+}
