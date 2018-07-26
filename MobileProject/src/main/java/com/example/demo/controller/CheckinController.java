7a04920573e7c203a5d7718119ca6058d0c2b823
rasyidmm <rasyidmaulidmajid@gmail.com>
2018-07-26 07:24:23 +0700

    ggwp

--- MobileProject/src/main/java/com/example/demo/controller/CheckinController.java
+++ MobileProject/src/main/java/com/example/demo/controller/CheckinController.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.*;
 import com.example.demo.service.*;
@@ -19,19 +16,27 @@ import de.odysseus.el.tree.impl.Parser.ParseException;
 public class CheckinController {
 	@Autowired
 	MasukService masukService;
-	 
+	@Autowired
+	AbsenService absenService;
+	@Autowired
+	DataLoginService dataLoginService;
 	 //Insert Masuk
 	@RequestMapping(value = "/insertmasuk/",method = RequestMethod.POST,headers="Accept=application/json")
 	public boolean insertMasuk(@RequestParam(value = "jam_masuk",required = false) @DateTimeFormat(pattern="HH:mm:dd") Date jam_masuk,
-	                                      @RequestParam (value = "lattitude_masuk", required = false) Float lattitude_masuk,
-	                                     @RequestParam(value = "longitude_masuk", required = false) Float longitude_masuk,
-	                                     @RequestParam(value = "status", required = false) Boolean status){
-		 Masuk masuk = new Masuk();
-		 masuk.setJam_masuk(jam_masuk);
+							   @RequestParam (value = "lattitude_masuk", required = false) Float lattitude_masuk,
+							   @RequestParam(value = "longitude_masuk", required = false) Float longitude_masuk,
+							   @RequestParam(value = "status", required = false) Boolean status, @ModelAttribute("Absen")Absen a,
+							   @ModelAttribute("Masuk")Masuk masuk){
+		
 		 masuk.setLattitude_masuk(lattitude_masuk);
 		 masuk.setLongitude_masuk(longitude_masuk);
 		 masuk.setStatus_fraud(status);
-		 masukService.SaveOrUpdate(masuk);
+		 masuk = masukService.SaveOrUpdate(masuk);
+		 a.setMasuk(masuk);
+		 a.setTanggal(new Date());
+		 a.setFoto_path("sdadsad");
+		 a.setTotal_jam_kerja(new Date());
+		 a.setDataLogin(dataLoginService.getById(1));
 	     return true;
 	}
 	 //Update Masuk
