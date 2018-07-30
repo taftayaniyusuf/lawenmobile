957e18c7a8eadb7bc44e4945ff257a58fecbc4c0
taftayaniyusuf <37141275+taftayaniyusuf@users.noreply.github.com>
2018-07-30 14:54:37 +0700

    rev2timegrou[

--- MobileProject/src/main/java/com/example/demo/controller/TimeGroupController.java
+++ MobileProject/src/main/java/com/example/demo/controller/TimeGroupController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,18 +20,26 @@ public class TimeGroupController {
     
    
     @RequestMapping(value = "/inserttimegroup",method = RequestMethod.POST,headers="Accept=application/json")
-    public boolean insertTimeGroup(@RequestParam("nama_timegroup") String nama_timegroup){
+    public boolean insertTimeGroup(@RequestParam("nama_timegroup") String nama_timegroup,
+    		@RequestParam("jammasuk")@DateTimeFormat(pattern="HH:mm:ss") Date jammasuk,
+    		@RequestParam("jamkeluar")@DateTimeFormat(pattern="HH:mm:ss") Date jamkeluar){
         TimeGroup timeGroup= new TimeGroup();
         timeGroup.setNama(nama_timegroup);
+        timeGroup.setJam_masuk(jammasuk);
+        timeGroup.setJam_keluar(jamkeluar);
         
         timeGroupService.SaveOrUpdate(timeGroup);
         return true;
     }
     @RequestMapping(value = "/updatetimegroup",method = RequestMethod.POST,headers="Accept=application/json")
-    public boolean updateTimeGroup(@RequestParam("id")long id,@RequestParam("nama_timegroup") String nama_timegroup){
+    public boolean updateTimeGroup(@RequestParam("id")long id,@RequestParam("nama_timegroup") String nama_timegroup,
+    		@RequestParam("jammasuk")@DateTimeFormat(pattern="HH:mm:ss") Date jammasuk,
+    		@RequestParam("jamkeluar")@DateTimeFormat(pattern="HH:mm:ss") Date jamkeluar){
         TimeGroup timeGroup= timeGroupService.getById(id);
         timeGroup.setId(id);
         timeGroup.setNama(nama_timegroup);
+        timeGroup.setJam_masuk(jammasuk);
+        timeGroup.setJam_keluar(jamkeluar);
         
         timeGroupService.SaveOrUpdate(timeGroup);
         return true;
@@ -50,17 +60,17 @@ public class TimeGroupController {
     
 //=============================================GET=============================================
     
-    @RequestMapping(value = "/timeGroupall",method = RequestMethod.GET,headers="Accept=application/json")
+    @RequestMapping(value = "/timegroupall",method = RequestMethod.GET,headers="Accept=application/json")
      public List<TimeGroup> timeGroupList(){
          return timeGroupService.getAllTimeGroup();
      }
    
-   @RequestMapping(value = "/timeGroupbyid",method = RequestMethod.GET)
+   @RequestMapping(value = "/timegroupbyid",method = RequestMethod.GET)
    public TimeGroup timeGroupbyid(@RequestParam("id")long id){
        return timeGroupService.getById(id);
    }
    
-   @RequestMapping(value = "/timeGroupaktif",method = RequestMethod.GET)
+   @RequestMapping(value = "/timegroupaktif",method = RequestMethod.GET)
    public List<TimeGroup> timeGroupbystatus(){
        return timeGroupService.getTimeGroupActive();
    }
