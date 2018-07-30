62afcad3eb7b53dad35075e28f15ca974b90658e
taftayaniyusuf <37141275+taftayaniyusuf@users.noreply.github.com>
2018-07-30 14:08:13 +0700

    rev timegroup

--- MobileProject/src/main/java/com/example/demo/service/TimeGroupService.java
+++ MobileProject/src/main/java/com/example/demo/service/TimeGroupService.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.TimeGroupDao;
 import com.example.demo.model.TimeGroup;
 import com.example.demo.repository.TimeGroupRepository;
 
+@Service
 public class TimeGroupService implements TimeGroupDao {
     @Autowired
     TimeGroupRepository timeGroupRepository;
